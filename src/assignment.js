0e0017ba7eddb3649bfa06c925d23c72dad7c3c6
Florence-makafui2 <73069746+Florence-makafui2@users.noreply.github.com>
2022-07-09 16:29:20 +0000

    Assignment Done, attempt 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,9 +13,14 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
+
+// function sumOfNumbers(arrayOfNumbers) {
+//     let sum = 0;
+//     for (i=0; i<arrayOfNumbers.length; i++){
+//         sum = sum + arrayOfNumbers[i]
+//     }
+//     return sum;
+// }
 // assignment.sumOfNumbers = sumOfNumbers;
 
 /**
@@ -27,10 +32,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
+
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0; 
+    for(i=0; i<arrayOfNumbers.length; i++){
+        if (arrayOfNumbers[i] % 2 ==0){
+            count ++
+        }
+    }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
